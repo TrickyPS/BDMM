2ff0e2e20b62fb93e3f348ea6461df80644ce0b6
Pedro Ramirez <pedroange1a@hotmail.com>
2021-09-05 19:21:24 -0500

    Update
    Update de varias paginas

---
+++ Proyecto BDM/JS/Cuenta.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+
+
+
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    $("#datetime").datetimepicker({
+        format: 'yyyy-mm-dd hh:ii'
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
